e98d7997e7cca92f16d60f77a7411fae0f75aaa0
Jakub Mamelski <jakub.mamelski@outlook.com>
2020-05-13 22:31:08 +0200

    Counting Duplicates dummy solution

---
+++ Codewars.dotnet/Kata/CountingDuplicates.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Kata
+{
+    // Write a function that will return the count of distinct case-insensitive alphabetic characters
+    // and numeric digits that occur more than once in the input string.
+    // The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+    public class CountingDuplicates
+    {
+        public static int DuplicateCount(string str)
+        {
+            var numberOfCharactersOccuringMoreThanOnce = 0;
+            var charactersOccurence = new HashSet<char>();
+            var duplicatesCounted = new HashSet<char>();
+            
+            foreach (var character in str.ToLower())
+            {
+                if (charactersOccurence.Contains(character))
+                {
+                    if (!duplicatesCounted.Contains(character))
+                    {
+                        ++numberOfCharactersOccuringMoreThanOnce;
+                        duplicatesCounted.Add(character);
+                    }
+                }
+                else
+                {
+                    charactersOccurence.Add(character);
+                }
+            }
+            return numberOfCharactersOccuringMoreThanOnce;
+        }
+    }
+}
